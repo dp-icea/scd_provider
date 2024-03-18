70f52d0e9ec03a7ced7536d2edb6cce339ff44f9
Rafael Albarello <rafael_albarello@hotmail.com>
2024-03-18 16:11:11 -0300

    Correcao url e portas

--- conflict_manager.py
+++ conflict_manager.py
@@ -1,5 +1,7 @@
 import requests
 
+from env import AUTH_URL
+
 
 class ConflictManager:
 
@@ -9,7 +11,7 @@ class ConflictManager:
         self.auth()
 
     def auth(self):
-        url = "http://localhost:8085/token?grant_type=client_credentials&intended_audience=localhost&issuer=localhost&scope={0}"
+        url = AUTH_URL + "/token?grant_type=client_credentials&intended_audience=localhost&issuer=localhost&scope={0}"
         self.constraint_management = requests.get(url.format("utm.constraint_management")).json()["access_token"]
 
     def check_restrictions(self, volume):
