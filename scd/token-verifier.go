08824be450bab88f7b4c98683880f13ce0376a14
Rafael Albarello <rafael_albarello@hotmail.com>
2024-07-01 09:09:07 -0300

    Added expected audience to token verification

--- scd/token-verifier.go
+++ scd/token-verifier.go
@@ -20,7 +20,7 @@ const (
 
 func (jwt JwtTokenVerifier) Verify(token string, expectedScope AuthScope) (bool, error) {
 	conf := config.GetGlobalConfig()
-	url := conf.TokenUrl + "?access_token=" + token + "&required_scope=" + string(expectedScope)
+	url := conf.TokenUrl + "?access_token=" + token + "&required_scope=" + string(expectedScope) + "&expected_audience=icea"
 	res, err := http.Get(url)
 	if err != nil {
 		return false, err
